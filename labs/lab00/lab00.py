2045a0cb1170b27fcf5b3f45e4a5cc550359cec6
Cynthia_woo <zhequnwu@gmail.com>
2023-09-18 23:22:28 -0700

    test

---
+++ labs/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_three():
+    """Come up with the most creative expression that evaluates to 2023,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_three()
+    2023
+    """
+    return (12 - 2) * 4 * 5 * 10 + 2 * 11 + 1; 
+
